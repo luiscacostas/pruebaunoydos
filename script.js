567e195be52fe4fc307f3d0920d0dff1f2694cf7
luiscacostas <acosta.luisc15@gmail.com>
2024-05-14 00:21:47 +0200

    utilizando prompt

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Diseña un algoritmo que cuente las veces que aparece una determinada letra en una frase
 
 let frase = 'Aprendiendo a programar en Javascript';
-let letra = 'a';
+let letra = prompt('Por Favor, elige una letra');
 let count = 0;
 
 for( let i = 0; i < frase.length; i++){
@@ -13,7 +13,10 @@ console.log(count);
 
 //Diseña un algoritmo que imprima los números impares entre un número dado por el usuario y los siguientes 50 números
 
-let numero = 20;
+//let numero = parseInt(prompt('introduzca un numero')); Asi lo solucionaria el error para utilizar prompt esta bien?
+
+let numero = 2;
+
 
 for(let i = numero; i < (numero + 50); i++){
     if(i % 2 !== 0){
